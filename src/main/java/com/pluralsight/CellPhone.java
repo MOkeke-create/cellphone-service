4819b2e8d13ca802cb582f8d301a8bd099edbcf8
MOkeke-create <Michaelokeke123@gmail.com>
2026-04-14 11:26:39 -0500

    Initial Commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class CellPhone {
+    private double serialNumber;
+    private String iPhoneModel;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public cellPhone(){
+
+    }
+
+    public double getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(double serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getiPhoneModel() {
+        return iPhoneModel;
+    }
+
+    public void setiPhoneModel(String iPhoneModel) {
+        this.iPhoneModel = iPhoneModel;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
+
