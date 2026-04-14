da641eff982d892eb995b10d4fd0e4fa695627d4
MOkeke-create <Michaelokeke123@gmail.com>
2026-04-14 11:26:32 -0500

    Initial Commit

---
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Cellphone cellphone = new Cellphone();
+        cellphone.setSerialNumber(0);
+
+
+    }
+}
